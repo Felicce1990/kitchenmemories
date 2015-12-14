8181669623345175f57805a97f097ed1901573ec
Felix Thomas <Felix Thomas@FelixThomas>
2015-12-14 15:35:08 +0100

    in manageRecipes you can add Recipes to a list. Will be saved to the recipe in the next step! FIXME: rendering AJAX not smooth.. workauround with render @all

--- src/java/de/felix/kitchenmemories/presentation/RecipeController.java
+++ src/java/de/felix/kitchenmemories/presentation/RecipeController.java
@@ -1,12 +1,16 @@
 package de.felix.kitchenmemories.presentation;
 
 import de.felix.kitchenmemories.business.IngredientService;
+import de.felix.kitchenmemories.business.NoSuchIngredientException;
 import de.felix.kitchenmemories.business.RecipeService;
 import de.felix.kitchenmemories.model.Ingredient;
+import de.felix.kitchenmemories.model.Ingredient_Recipe;
 import java.io.Serializable;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -17,29 +21,62 @@ public class RecipeController implements Serializable {
 
     private String name;
     private String description;
-    private List<Ingredient> ingredients;
+    private Ingredient ingredients;
     private String selectedIngredient;
     private double amount;
     private String unit;
     private double amountOfPeople;
+    private List<Ingredient_Recipe> ingredient_Recipes;
 
     private Map<String, Object> mappedIngredients;
 
     @EJB
     private RecipeService service;
-    
+
     @EJB
     private IngredientService iService;
 
-    public void printIngredient()
-    {
+    @PostConstruct
+    public void init() {
+        ingredient_Recipes = new LinkedList<>();
+    }
+
+    public void printIngredient() {
         System.out.println("### amount: " + amount);
         System.out.println("### unit: " + unit);
         System.out.println("### ingredient: " + selectedIngredient);
     }
+
+    public void collectIngredients() throws NoSuchIngredientException {
+
+        System.out.println("Es wird hinzugefügt:");
+        printIngredient();
+        Ingredient_Recipe ir = new Ingredient_Recipe();
+        ir.setUnit(unit);
+        ir.setAmount(amount);
+        ir.setIngredient(iService.findByName(selectedIngredient));
+        System.out.println("Hinzugrfügt:");
+        System.out.println("amount: " + ir.getAmount());
+        System.out.println("unit: " + ir.getUnit());
+        System.out.println("Ingredient: " + ir.getIngredient().getName());
+        System.out.println("Added to list!");
+        ingredient_Recipes.add(ir);
+    }
     
+    public void removeIngredientFromList(Ingredient_Recipe ir)
+    {
+        System.out.println("trying to remove!");
+        System.out.println("List contains:");
+        for(int i = 0; i < ingredient_Recipes.size(); i++)
+        {
+            System.out.println("Contains: " + ingredient_Recipes.get(i).getIngredient().getName());
+        }
+        System.out.println("remove: " + ir.getIngredient().getName());
+        ingredient_Recipes.remove(ir);
+    }
+
     public Map<String, Object> getMappedIngredients() {
-        
+
         List<Ingredient> list = iService.findAll();
         mappedIngredients = new LinkedHashMap<String, Object>();
 
@@ -49,6 +86,14 @@ public class RecipeController implements Serializable {
         return mappedIngredients;
     }
 
+    public List<Ingredient_Recipe> getIngredient_Recipes() {
+        return ingredient_Recipes;
+    }
+
+    public void setIngredient_Recipes(List<Ingredient_Recipe> ingredient_Recipes) {
+        this.ingredient_Recipes = ingredient_Recipes;
+    }
+
     public String getSelectedIngredient() {
         return selectedIngredient;
     }
@@ -56,7 +101,6 @@ public class RecipeController implements Serializable {
     public void setSelectedIngredient(String selectedIngredient) {
         this.selectedIngredient = selectedIngredient;
     }
-
 
     public double getAmount() {
         return amount;
@@ -82,11 +126,11 @@ public class RecipeController implements Serializable {
         this.description = description;
     }
 
-    public List<Ingredient> getIngredients() {
+    public Ingredient getIngredients() {
         return ingredients;
     }
 
-    public void setIngredients(List<Ingredient> Ingredients) {
+    public void setIngredients(Ingredient Ingredients) {
         this.ingredients = Ingredients;
     }
 
